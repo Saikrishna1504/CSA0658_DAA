d127586619103c4d2730d2429b8497a2bd11d9cd
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-08-06 22:24:24 +0530

    Update Copy String to Another.c

--- 40_Program/Copy String to Another.c
+++ 40_Program/Copy String to Another.c
@@ -1,19 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+	char s1[20] = "Saikrishna";
+	char s2[20];
+	
+	sprintf(s2, "%s", s1);
 
-void copy(char s1[], char s2[],int i){
-	if(s1[i]=='\0'){
-		return;
-	}
-	s2[i]=s1[i];
-	copy(s1,s2,i+1);
-}
+	printf("%s\n", s1);
 
-int main(){
-	char s[100],scpy[100];
-	printf("Enter a string : ");
-	scanf("%s",s);
-	copy(s,scpy,0);
-	printf("The original string is : %s\n",s);
-	printf("The copied string is : %s\n",scpy);
 	return 0;
 }
