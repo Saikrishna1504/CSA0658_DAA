41b250ed2f0d5366990011dc1b6279941855b50b
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-08-06 20:31:48 +0530

    Update Binary Coefficient using Dynamic Programming.c

--- 40_Program/Binary Coefficient using Dynamic Programming.c
+++ 40_Program/Binary Coefficient using Dynamic Programming.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-int binomialCoeff(int n, int k) {
-    int C[n+1][k+1];
-    int i, j;
-
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= (i < k ? i : k); j++) {
-            if (j == 0 || j == i)
-                C[i][j] = 1;
-            else
-                C[i][j] = C[i-1][j-1] + C[i-1][j];
-        }
-    }
-
-    return C[n][k];
-}
 
 int main() {
     int n, k;
-
+    
+    // Prompt the user to enter values for n and k
     printf("Enter n: ");
     scanf("%d", &n);
     printf("Enter k: ");
     scanf("%d", &k);
 
-    printf("Binomial Coefficient C(%d, %d) is %d\n", n, k, binomialCoeff(n, k));
+    // Declare a 2D array to store binomial coefficients
+    int C[n+1][k+1];
+    int i, j;
+
+    // Compute binomial coefficients using dynamic programming
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= k; j++) {
+            // Base cases: C(i, 0) = 1 and C(i, i) = 1
+            if (j == 0 || j == i)
+                C[i][j] = 1;
+            // If j is greater than i, it should be 0
+            else if (j > i)
+                C[i][j] = 0;
+            // Use the recursive formula: C(i, j) = C(i-1, j-1) + C(i-1, j)
+            else
+                C[i][j] = C[i-1][j-1] + C[i-1][j];
+        }
+    }
+
+    // Print the result
+    printf("Binomial Coefficient C(%d, %d) is %d\n", n, k, C[n][k]);
 
     return 0;
 }
-
